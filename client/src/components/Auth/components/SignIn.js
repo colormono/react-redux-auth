cf9c8cdedd0c656dfe7519126bf70daff6bdd3fb
COLORMONO <colormono@gmail.com>
2019-02-12 18:53:00 -0300

    Moved SignIn from ReduxForm to Formik

--- client/src/components/Auth/components/SignIn.js
+++ client/src/components/Auth/components/SignIn.js
@@ -1,41 +1,62 @@
 import React from 'react';
-import { reduxForm, Field } from 'redux-form';
-import { compose } from 'redux';
 import { connect } from 'react-redux';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import signIn from '../actions/signIn';
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+  password: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required')
+});
+
 
 class SignIn extends React.Component {
   onSubmit = formProps => {
+    console.log(formProps);
     this.props.handleFormSubmit(formProps, () => {
       this.props.history.push('/features');
     });
   };
 
   render() {
-    const { handleSubmit } = this.props; // provided by redux-form
-
     return (
-      <form onSubmit={handleSubmit(this.onSubmit)} className="ui form">
-        <div className="field">
-          <label>Email</label>
-          <Field
-            name="email"
-            type="text"
-            component="input"
-            autoComplete="none"
-          />
-        </div>
-        <div className="field">
-          <label>Password</label>
-          <Field name="password" type="password" component="input" />
-        </div>
-        <div>{this.props.errorMessage}</div>
-        <div className="field">
-          <button type="submit" className="ui button">
-            Sign Up
-          </button>
-        </div>
-      </form>
+      <Formik
+        initialValues={{ email: 'asd@asd.com', password: '' }}
+        onSubmit={this.onSubmit}
+        validationSchema={SignupSchema}
+        render={props => (
+          <Form className="ui form">
+            <div className="field">
+              <label>Email</label>
+              <Field name="email" type="email" autoComplete="none" />
+              {props.errors.email && props.touched.email ? (
+                <div>{props.errors.email}</div>
+              ) : null}
+            </div>
+
+            <div className="field">
+              <label>Password</label>
+              <Field name="password" type="password" />
+              {props.errors.password && props.touched.password ? (
+                <div>{props.errors.password}</div>
+              ) : null}
+            </div>
+
+            <div>{this.props.errorMessage}</div>
+
+            <div className="field">
+              <button type="submit" className="ui button">
+                Sign Up
+              </button>
+            </div>
+          </Form>
+        )}
+      />
     );
   }
 }
@@ -52,10 +73,7 @@ const mapDispatchToProps = dispatch => ({
   }
 });
 
-export default compose(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  ),
-  reduxForm({ form: 'SignIn' })
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
 )(SignIn);
